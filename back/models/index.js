ab05dd72d620489bf7831b04da02d468bc3f12d8
bigkangtheory <lisalkang@gmail.com>
2017-05-09 18:11:33 -0400

    dev env

--- back/models/index.js
+++ back/models/index.js
@@ -8,10 +8,10 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
+if (process.env.NODE_ENV === 'development') {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+} else {
+  var sequelize = new Sequelize(process.env.DATABASE_URL);
 }
 
 fs
