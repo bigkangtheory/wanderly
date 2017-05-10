b831090a19aa4b5558b7eb95b4458a5e4bd26775
bigkangtheory <lisalkang@gmail.com>
2017-05-10 13:24:28 -0400

    dialect postgres move

--- back/models/index.js
+++ back/models/index.js
@@ -5,10 +5,10 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (process.env.NODE_ENV === 'development' || !process.env.DATABASE_URL) {
+  var config    = require(__dirname + '/../config/config.json')[env];
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 } else {
   var sequelize = new Sequelize(process.env.DATABASE_URL, {
